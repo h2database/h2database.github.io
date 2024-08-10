c0f8763b6b1c518d5340e9bd7851adf38376abcf
Andrei Tokar <andrei.tokar@gmail.com>
2024-08-10 17:15:37 -0400

    2.3.232

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.h2.api"},{"l":"org.h2.engine"},{"l":"org.h2.fulltext"},{"l":"org.h2.jdbc"},{"l":"org.h2.jdbcx"},{"l":"org.h2.tools"}];updateSearchResults();
